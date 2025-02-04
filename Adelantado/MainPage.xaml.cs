6a8664220afafc1e3eeb09b8aa995f78d23d9c17
beto200519 <norbertocarrillo2005@gmail.com>
2025-02-04 09:00:09 -0700

    avance pal miguelon

---
+++ Adelantado/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace Adelantado
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MostrarCamposButton_Clicked(object sender, System.EventArgs e)
+        {
+            CamposOcultos.IsVisible = true;
+        }
+
+        private void AgregarUbicacionButton_Clicked(object sender, System.EventArgs e)
+        {
+            string direccion = DireccionCompletaEntry.Text;
+            string codigoDispenser = CodigoDispenserEntry.Text;
+            string idTecnico = IdTecnicoEntry.Text;
+            string idSupervisor = IdSupervisorEntry.Text;
+
+            DisplayAlert("Éxito", "Ubicación agregada correctamente", "OK");
+
+            CamposOcultos.IsVisible = false;
+        }
+
+        private void CancelarButton_Clicked(object sender, System.EventArgs e)
+        {
+            CamposOcultos.IsVisible = false;
+        }
+    }
+}
---
+++ Adelantado/obj/Debug/net8.0-android/android/src/crc642c31cf59d4aef40c/MainActivity.java
@@ -0,0 +1,48 @@
+package crc642c31cf59d4aef40c;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("Adelantado.MainActivity, Adelantado", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Adelantado.MainActivity, Adelantado", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("Adelantado.MainActivity, Adelantado", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
